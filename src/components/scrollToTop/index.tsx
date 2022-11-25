357e9e50966f6b46452eb43053657d25b71de543
Yael Ibarra <ibgayael@gmail.com>
2022-11-24 21:27:21 -0600

    Added about me section

--- src/components/scrollToTop/index.tsx
+++ src/components/scrollToTop/index.tsx
@@ -21,14 +21,18 @@ const ScrollToTop = () => {
       behavior: "smooth",
     });
   };
+ 
   return (
     <div>
       {showScrollTopButton && (
-        <div className="top-btn-container" onClick={scrollTop}>
+        <div
+          className="top-btn-container"
+          onClick={scrollTop}
+        >
           <img
             src={heart}
             alt="Heart"
-            className="top-btn-style"
+            className="top-btn-img"
           />
         </div>
       )}
